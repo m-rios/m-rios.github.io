8e40aae22a750c1ef80b7aba92b4e6cf76c03cc5
Mario <mrm994@gmail.com>
2016-12-01 11:16:58 +0100

    Added working jquery logic

--- resources/main.js
+++ resources/main.js
@@ -1,14 +1,30 @@
-$(function() {
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				}, 800);
-				return false;
+$(document).ready(function(){
+	var $ = jQuery.noConflict();
+
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 800);
+					return false;
+				}
 			}
+		});
+	});
+
+	var landing_end = $("#landing").height() - $(".header").height();
+
+	$(window).on("scroll", function () {
+		if ($(this).scrollTop() > landing_end ) {
+			$("#puta").addClass("fill");
+		}
+		else {
+			$("#puta").removeClass("fill");
 		}
 	});
+
 });
