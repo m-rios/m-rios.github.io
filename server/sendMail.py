7fd45721fe86f67e932d09c2e338cad5c57b9b55
Mario <mrm994@gmail.com>
2016-12-05 10:52:32 +0100

    Added contact form to main page

---
+++ server/sendMail.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+
+import json
+from smtplib import SMTP
+
+print('Loading function')
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
+    access to the request and response payload, including headers and
+    status code.
+
+    To scan a DynamoDB table, make a GET request with the TableName as a
+    query string parameter. To put, update, or delete an item, make a POST,
+    PUT, or DELETE request respectively, passing in the payload to the
+    DynamoDB API as a JSON body.
+    '''
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    operations = ['POST']
+
+    operation = event['httpMethod']
+    if operation in operations:
+        pass
+    else:
+        return respond(ValueError('Unsupported method "{}"'.format(operation)))
