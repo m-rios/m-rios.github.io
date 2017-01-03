c2146a04966647c16514df8d09521d90b33a1221
Mario <mrm994@gmail.com>
2017-01-03 18:21:29 +0100

    Some content fixes and icon added to sidebar

--- assets/js/form-logic.js
+++ assets/js/form-logic.js
@@ -1,10 +1,20 @@
-function submitForm(form) {
-		console.log("putas");
+// function submitForm(form) {
+// 		console.log("putas");
 
-		var data = $(this).serializeArray().reduce(function(obj, item) {
-			if (item.value == ""){	return; }
-			obj[item.name] = item.value;
-			return obj;
-		}, {});
+// 		var data = $(this).serializeArray().reduce(function(obj, item) {
+// 			if (item.value == ""){	return; }
+// 			obj[item.name] = item.value;
+// 			return obj;
+// 		}, {});
 
-	}
+// 	}
+
+
+$('#form').on('submit', function () {
+	var data = [];
+	data["name"] = $('#name').val();
+	data["email"] = $('#email').val();
+	data["msg"] = $('#message').val();
+
+	jQuery.post("https://v1wesefrvk.execute-api.eu-central-1.amazonaws.com/prod/sendContactForm", data, "json");
+})
